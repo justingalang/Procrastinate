c24579b6689f87acd9b69b490bdfe2d0df3f893c
Justin Galang <justingalang@gmail.com>
2020-09-23 18:32:32 -0500

    Initial Commit

---
+++ Procrastinate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Procrastinate
+//
+//  Created by Justin Galang on 9/23/20.
+//  Copyright © 2020 Justin Galang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
